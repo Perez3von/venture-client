83ad0b0049e7570c59c30e6471e8aa6e9d413c46
Evon Perez <perez.evon@gmail.com>
2022-04-29 08:20:47 -0400

    added env vars

--- src/views/Venture.jsx
+++ src/views/Venture.jsx
@@ -78,7 +78,7 @@ export default function Venture(){
             oldMessages();
         };
         // socket.current = io('ws://localhost:8900');
-        socket.current = io(`https://venturechatsocket.herokuapp.com/ `);
+        socket.current = io(`ws://https://venturechat.herokuapp.com`);
         socket.current.emit('create', {
                 room: room });
     },[id, navigate]);
