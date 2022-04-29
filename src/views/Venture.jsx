ffaa6e4bb1c08c219f3123be90c893634535b9a2
Evon Perez <perez.evon@gmail.com>
2022-04-29 12:12:32 -0400

    troubleshoting socket

--- src/views/Venture.jsx
+++ src/views/Venture.jsx
@@ -77,10 +77,11 @@ export default function Venture(){
             };
             oldMessages();
         };
+        socket.current = io('ws://https://venturechatsocket.herokuapp.com');
         // socket.current = io('ws://localhost:8900');
-        socket.current = io(`ws:https://venturechatsocket.herokuapp.com `,{
-            withCredentials: true
-        });
+        // socket.current = io('ws://localhost:8900 ',{
+        //     withCredentials: true
+        // });
         socket.current.emit('create', {
                 room: room });
     },[id, navigate]);
